70837285a1fb9445eddbbb8b662818fe16a3a394
Inna Primova <mmf.inna@gmail.com>
2021-04-17 00:10:04 +0300

    added GmailServiceTest

--- server-recipe-book/src/test/java/by/bsu/recipebook/service/email/GmailServiceTest.java
+++ server-recipe-book/src/test/java/by/bsu/recipebook/service/email/GmailServiceTest.java
@@ -1,4 +1,47 @@
-import static org.testng.Assert.*;
+package by.bsu.recipebook.service.email;
+
+import by.bsu.recipebook.dto.EmailDto;
+import by.bsu.recipebook.email.GoogleMail;
+import by.bsu.recipebook.mapper.EmailMapper;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
+
 public class GmailServiceTest {
-    
+    private AutoCloseable closeable;
+
+    @Mock
+    private EmailMapper emailMapper;
+
+    @Mock
+    private GmailService gmailService;
+
+    @BeforeMethod
+    public void setUp() {
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    public void shouldSendMessageToCompany() {
+        GoogleMail googleMail = mock(GoogleMail.class);
+        EmailDto emailDto = new EmailDto("test@email.com", "Test", "Test message");
+        when(emailMapper
+                .mapToGoogleMail(emailDto))
+                .thenReturn(new GoogleMail("test@email.com", "Test", "Test message"));
+        doNothing()
+                .when(googleMail)
+                .sendMessage();
+        doNothing()
+                .when(gmailService)
+                .sendMessageToCompany(emailDto);
+    }
 }
