7bae2aeba28a1b9f66e844aa4f5b79a1a99d5ba8
Inna Primova <mmf.inna@gmail.com>
2021-04-25 23:30:53 +0300

    added AuthServiceTest

--- server-recipe-book/src/test/java/by/bsu/recipebook/service/AuthServiceTest.java
+++ server-recipe-book/src/test/java/by/bsu/recipebook/service/AuthServiceTest.java
@@ -9,7 +9,6 @@ import by.bsu.recipebook.dto.auth.RegisterRequest;
 import by.bsu.recipebook.entity.NotificationEmail;
 import by.bsu.recipebook.entity.RefreshToken;
 import by.bsu.recipebook.entity.User;
-import by.bsu.recipebook.entity.VerificationToken;
 import by.bsu.recipebook.exception.ServiceException;
 import by.bsu.recipebook.mapper.UserMapper;
 import by.bsu.recipebook.repository.RoleRepository;
@@ -21,6 +20,7 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -28,23 +28,19 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.powermock.api.support.membermodification.MemberMatcher.method;
 
+@PowerMockIgnore({"*.*"})
 @PrepareForTest({AuthService.class})
-public class AuthServiceTestWithPrivate {
+public class AuthServiceTest {
     private AutoCloseable closeable;
 
     @Mock
@@ -82,24 +78,21 @@ public class AuthServiceTestWithPrivate {
 
     private AuthService authService;
 
-    private static final String FIRST_PRIVATE_METHOD = "fetchUserAndEnable";
+    private static final String PRIVATE_METHOD_GENERATE_VERIFICATION_TOKEN = "generateVerificationToken";
 
-    private static final String SECOND_PRIVATE_METHOD = "generateVerificationToken";
+    private final String username = "email@gmail.com";
 
-    private final User user = new User();
+    private final String fullName = "John Smith";
 
     @Captor
     private ArgumentCaptor<User> userArgumentCaptor;
 
-    @BeforeTest
+    @BeforeMethod
     public void setUp() {
         closeable = MockitoAnnotations.openMocks(this);
         authService = new AuthService(passwordEncoder, userRepository, roleRepository,
                 userMapper, verificationTokenRepository, mailService,
                 authenticationManager, jwtProvider, refreshTokenService);
-        user.setIdUser(1);
-        user.setEmail("email@gmail.com");
-        user.setFullName("John Smith");
     }
 
     @AfterMethod
@@ -115,42 +108,31 @@ public class AuthServiceTestWithPrivate {
 
     @Test
     public void shouldSignup() throws Exception {
+        User user = new User();
+        user.setIdUser(1);
+        user.setEmail(username);
+        user.setFullName(fullName);
         RegisterRequest registerRequest =
-                new RegisterRequest("email@gmail.com", null, "John Smith", null);
+                new RegisterRequest(username, null, fullName, null);
         when(userRepository.existsByEmail(anyString())).thenReturn(false);
         when(passwordEncoder.encode(anyString())).thenReturn("pass");
         when(userMapper.mapToUser(any(RegisterRequest.class))).thenReturn(user);
-        //AuthService spy = PowerMockito.spy(authService);
-        PowerMockito.when(authService, method(AuthService.class, SECOND_PRIVATE_METHOD, User.class))
+        PowerMockito.when(authService, method(AuthService.class, PRIVATE_METHOD_GENERATE_VERIFICATION_TOKEN, User.class))
                 .withArguments(any(User.class))
                 .thenReturn("Some string");
         doNothing().when(mailService).sendMail(any(NotificationEmail.class));
         authService.signup(registerRequest);
         verify(userRepository, times(1)).save(userArgumentCaptor.capture());
         assertThat(userArgumentCaptor.getValue().getIdUser()).isEqualTo(1);
-        assertThat(userArgumentCaptor.getValue().getFullName()).isEqualTo("John Smith");
-        PowerMockito.verifyPrivate(PowerMockito.spy(authService), times(1))
-                .invoke(eq(SECOND_PRIVATE_METHOD), user);
-
-    }
-
-    @Test()
-    public void shouldVerifyAccount() throws Exception {
-        String token = UUID.randomUUID().toString();
-        VerificationToken verificationToken = new VerificationToken(1, token, null, user);
-        when(verificationTokenRepository.findByToken(anyString()))
-                .thenReturn(Optional.of(verificationToken));
-        AuthService spy = PowerMockito.spy(authService);
-        PowerMockito.doNothing()
-                .when(spy, method(AuthService.class, FIRST_PRIVATE_METHOD, VerificationToken.class));
-        spy.verifyAccount(token);
-        when(userRepository.findByEmail(eq("email@gmail.com"))).thenReturn(user);
-        PowerMockito.verifyPrivate(spy, times(1))
-                .invoke(eq(FIRST_PRIVATE_METHOD), verificationToken);
+        assertThat(userArgumentCaptor.getValue().getFullName()).isEqualTo(fullName);
     }
 
     @Test(groups = {"security"})
     public void shouldGetCurrentUser() {
+        User user = new User();
+        user.setIdUser(1);
+        user.setEmail(username);
+        user.setFullName(fullName);
         CustomUserDetails customUserDetails = CustomUserDetails.fromUserToCustomUserDetails(user);
         when(authentication.getPrincipal()).thenReturn(customUserDetails);
         when(userRepository.findByEmail(customUserDetails.getUsername())).thenReturn(user);
@@ -162,7 +144,6 @@ public class AuthServiceTestWithPrivate {
     public void shouldRefreshToken() throws ServiceException {
         String authenticationToken = UUID.randomUUID().toString();
         String refreshToken = UUID.randomUUID().toString();
-        String username = "email@gmail.com";
         AuthenticationResponse expected = new AuthenticationResponse(authenticationToken, username, refreshToken, null, null, null);
         RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest(refreshToken, username);
         when(refreshTokenService.validateRefreshToken(refreshToken)).thenReturn(true);
@@ -180,7 +161,10 @@ public class AuthServiceTestWithPrivate {
 
     @Test
     public void shouldLogin() {
-        String username = "email@gmail.com";
+        User user = new User();
+        user.setIdUser(1);
+        user.setEmail(username);
+        user.setFullName(fullName);
         LoginRequest loginRequest = new LoginRequest(username, null);
         String authenticationToken = UUID.randomUUID().toString();
         String refreshToken = UUID.randomUUID().toString();
